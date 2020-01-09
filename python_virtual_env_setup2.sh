538d0e66e0a0787dbb7834c0af194562bfa5aebc
spyrouath <spyroua@gmail.com>
2020-01-09 18:38:08 +0200

    Update python_virtual_env_setup2.sh

--- python_virtual_env_setup2.sh
+++ python_virtual_env_setup2.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-mkdir venv
-sudo virtualenv -p /usr/local/bin/python3.7 venv
+sudo virtualenv -p /usr/local/bin/python3.7 /home/sshuser/venv
 sudo chmod 755 /home/sshuser/venv
 sudo /home/sshuser/venv/bin/pip3.7 install -r /home/sshuser/venv_requirements.txt
