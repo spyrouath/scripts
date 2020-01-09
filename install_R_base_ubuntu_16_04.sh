8abf636a77ab81fce4b3ab8ff15004219648800d
spyrouath <spyroua@gmail.com>
2020-01-09 11:41:45 +0200

    Update install_R_base_ubuntu_16_04.sh

--- install_R_base_ubuntu_16_04.sh
+++ install_R_base_ubuntu_16_04.sh
@@ -2,8 +2,10 @@
 
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys E298A3A825C0D65DFD57CBB651716619E084DAB9
 
-sudo add-apt-repository 'deb [arch=amd64,i386] https://cran.rstudio.com/bin/linux/ubuntu xenial/'
+sudo apt autoremove -y
 
-sudo apt-get update
+sudo add-apt-repository 'deb [arch=amd64,i386] https://cran.rstudio.com/bin/linux/ubuntu xenial/' -y
 
-sudo apt-get install r-base
+sudo apt-get update -y
+
+sudo apt-get install r-base -y
