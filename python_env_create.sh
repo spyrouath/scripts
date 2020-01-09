6f2a5a915450027e84a3551c4d3c5180665716c5
spyrouath <spyroua@gmail.com>
2020-01-09 13:05:27 +0200

    Update python_env_create.sh

--- python_env_create.sh
+++ python_env_create.sh
@@ -1,8 +1,20 @@
 #!/bin/bash
 
+# Install python 3.7
+sudo apt install build-essential zlib1g-dev libncurses5-dev libgdbm-dev libnss3-dev libssl-dev libreadline-dev libffi-dev wget -y
+cd /tmp
+wget https://www.python.org/ftp/python/3.7.2/Python-3.7.2.tar.xz
+tar -xf Python-3.7.2.tar.xz
+cd Python-3.7.2
+./configure --enable-optimizations
+sudo make altinstall
+
+# Create python virtual environmant
 sudo apt install virtualenv -y
 apt install wget -y
 wget --no-check-certificate --content-disposition https://raw.githubusercontent.com/spyrouath/scripts/master/venv_requirements.txt
-virtualenv -p /usr/bin/anaconda/envs/py35/bin/python3 venv
+rm -rf venv
+virtualenv -p /usr/local/bin/python3.7 venv
+sudo chmod 755 /home/sshuser/venv
 source /home/sshuser/venv/bin/activate
 sudo pip install -r /home/sshuser/venv_requirements.txt
