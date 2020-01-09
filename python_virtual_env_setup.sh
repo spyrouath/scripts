ab3f3f149f5e737b0f2629eab68d890c8fbda66b
spyrouath <spyroua@gmail.com>
2020-01-09 15:59:54 +0200

    Create python_virtual_env_setup.sh

---
+++ python_virtual_env_setup.sh
@@ -0,0 +1,8 @@
+!/bin/bash
+
+# Create python virtual environmant
+sudo apt install virtualenv -y
+wget --no-check-certificate --content-disposition https://raw.githubusercontent.com/spyrouath/scripts/master/venv_requirements.txt -P /home/sshuser
+sudo virtualenv -p /usr/local/bin/python3.7 venv
+sudo chmod 755 /home/sshuser/venv
+sudo /home/sshuser/venv/bin/pip3.7 install -r /home/sshuser/venv_requirements.txt
