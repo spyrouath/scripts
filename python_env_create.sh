2e5460ea143cc4aa719bc1b5a3a70c59303723bb
spyrouath <spyroua@gmail.com>
2020-01-09 12:43:45 +0200

    Update python_env_create.sh

--- python_env_create.sh
+++ python_env_create.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-sudo apt install virtualenv
+sudo apt install virtualenv -y
+apt install wget -y
+wget https://github.com/spyrouath/scripts/blob/master/venv_requirements.txt
 virtualenv -p /usr/bin/anaconda/envs/py35/bin/python3 venv
 source /home/sshuser/venv/bin/activate
-sudo /home/sshuser/venv/bin/pip install -e https://github.com/spyrouath/scripts/blob/master/venv_requirements.txt
+sudo /home/sshuser/venv/bin/pip install -r /home/sshuser/venv_requirements.txt
