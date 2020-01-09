629c9dac0f2b602e8f62548ed09c689a864008e5
spyrouath <spyroua@gmail.com>
2020-01-09 11:35:37 +0200

    Update install_R_base_ubuntu_16_04.sh
    Add #!/bin/bash to bash script

--- install_R_base_ubuntu_16_04.sh
+++ install_R_base_ubuntu_16_04.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys E298A3A825C0D65DFD57CBB651716619E084DAB9
 
 sudo add-apt-repository 'deb [arch=amd64,i386] https://cran.rstudio.com/bin/linux/ubuntu xenial/'
