1b14c834189a03d7b76e405bdcbb7ec5dbbfe0f3
spyrouath <spyroua@gmail.com>
2020-01-10 16:02:54 +0200

    Delete python374_env_create.sh

--- python374_env_create.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-wget --no-check-certificate --content-disposition https://www.python.org/ftp/python/3.7.4/Python-3.7.4.tar.xz -P /home/sshuser
-sudo chmod 755 /home/sshuser/Python-3.7.4.tar.xz
-sudo tar -xf /home/sshuser/Python-3.7.4.tar.xz -C /home/sshuser
