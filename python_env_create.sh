b61cef4d85888bfd5a9aa1e20d6f215dfea37ca3
spyrouath <spyroua@gmail.com>
2020-01-10 15:46:18 +0200

    Create python_env_create.sh

---
+++ python_env_create.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Install python 3.7.4
+sudo apt install build-essential zlib1g-dev libbz2-dev libncurses5-dev libgdbm-dev libnss3-dev libssl-dev libreadline-dev libsqlite3-dev tk-dev libncurses5-dev libffi-dev wget -y
+wget --no-check-certificate --content-disposition https://www.python.org/ftp/python/3.7.4/Python-3.7.4.tar.xz -P /home/sshuser
+tar -xf /home/sshuser/Python-3.7.4.tar.xz
+cd /home/sshuser/Python-3.7.4
+./configure --enable-optimizations --enable-loadable-sqlite-extensions
+sudo make altinstall
