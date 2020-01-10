1aa3d2eb2403b4a7bdbe207d8940cc6fb87edf09
spyrouath <spyroua@gmail.com>
2020-01-10 13:54:51 +0200

    Update python_install_requirements.sh

--- python_install_requirements.sh
+++ python_install_requirements.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
+
+sudo rm /home/sshuser/venv_requirements.txt
+
+wget --no-check-certificate --content-disposition https://raw.githubusercontent.com/spyrouath/scripts/master/venv_requirements.txt -P /home/sshuser
+
 sudo /home/sshuser/venv/bin/pip3.7 install -r /home/sshuser/venv_requirements.txt
