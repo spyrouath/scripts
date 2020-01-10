616fa038e8d09a047ce9dbdaaf0ab9590322d618
spyrouath <spyroua@gmail.com>
2020-01-10 17:03:47 +0200

    Update install_R_packages.R

--- install_R_packages.R
+++ install_R_packages.R
@@ -1,3 +1,16 @@
+#!/bin/bash
+
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys E298A3A825C0D65DFD57CBB651716619E084DAB9
+
+sudo apt autoremove -y
+
+sudo add-apt-repository 'deb [arch=amd64,i386] https://cran.rstudio.com/bin/linux/ubuntu xenial/' -y
+
+sudo apt-get update -y
+
+sudo apt-get install r-base -y
+
+sudo R -e '''
 install.packages("readxl")
 install.packages("data.table")
 install.packages("stringr")
@@ -20,3 +33,4 @@ install.packages("rowr")
 install.packages("pracma")
 install.packages("pdc")
 install.packages("reticulate")
+'''
