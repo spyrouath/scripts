a64417fbe86544f8a1df51bff46ab3e1157798a2
spyrouath <spyroua@gmail.com>
2020-01-10 15:42:54 +0200

    Update python_env_create.sh

--- python_env_create.sh
+++ python_env_create.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 # Install python 3.7.4
-wget https://www.python.org/ftp/python/3.7.4/Python-3.7.4.tar.xz -P /home/sshuser
+wget --no-check-certificate --content-disposition https://www.python.org/ftp/python/3.7.4/Python-3.7.4.tar.xz -P /home/sshuser
 
