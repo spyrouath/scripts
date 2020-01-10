b516e3863a005b3c096fdf84434ead6f810b8f5d
spyrouath <spyroua@gmail.com>
2020-01-10 19:43:58 +0200

    Update temp.sh

--- temp.sh
+++ temp.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-echo "deb https://cloud.r-project.org/bin/linux/ubuntu xenial-cran35/" >> /etc/apt/sources.list
-
-sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys E298A3A825C0D65DFD57CBB651716619E084DAB9
+sudo bash -c 'echo "deb https://cloud.r-project.org/bin/linux/ubuntu xenial-cran35/" >> /etc/apt/sources.list' && sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys E298A3A825C0D65DFD57CBB651716619E084DAB9 && sudo apt update
 
 sudo apt-get install r-base-dev -y
