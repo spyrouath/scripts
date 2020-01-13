89a43e082c9ba552f045f02f77f150d8d81f9263
spyrouath <spyroua@gmail.com>
2020-01-13 13:03:17 +0200

    Delete temp.sh

--- temp.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo "deb https://cloud.r-project.org/bin/linux/ubuntu xenial-cran35/" >> /etc/apt/sources.list
-
-sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys E298A3A825C0D65DFD57CBB651716619E084DAB9
-
-sudo apt-get install r-base-dev -y
