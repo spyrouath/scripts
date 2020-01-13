e2991cdbb0fee1ffaade0fc7e989b74ec337cb47
spyrouath <spyroua@gmail.com>
2020-01-13 18:20:46 +0200

    Update temp.sh

--- temp.sh
+++ temp.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-sudo dpkg --configure -a
+# sudo dpkg --configure -a
+
+sudo rm /var/lib/apt/lists/lock
+sudo rm /var/cache/apt/archives/lock
+cd /var/lib/dpkg/updates
+sudo rm *
+sudo apt-get update
 
 sudo apt update -y
 sudo apt upgrade -y
