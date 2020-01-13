ebef6efaba083ac041fe11451c2d701c014fdd14
spyrouath <spyroua@gmail.com>
2020-01-13 11:30:04 +0200

    Update installR36.sh

--- installR36.sh
+++ installR36.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 
-echo "deb https://cloud.r-project.org/bin/linux/ubuntu xenial-cran35/" >> /etc/apt/sources.list
-
-sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys E298A3A825C0D65DFD57CBB651716619E084DAB9
+sudo bash -c 'echo "deb https://cloud.r-project.org/bin/linux/ubuntu xenial-cran35/" >> /etc/apt/sources.list' && sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys E298A3A825C0D65DFD57CBB651716619E084DAB9 && sudo apt update
 
 sudo apt-get install r-base-dev -y
-
 
 sudo R -e "install.packages('readxl')"
 sudo R -e "install.packages('data.table')"
