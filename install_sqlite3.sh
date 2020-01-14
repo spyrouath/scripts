32ed605dc7b639a60ce0a45695d8589106f0613f
spyrouath <spyroua@gmail.com>
2020-01-14 18:56:20 +0200

    Create install_sqlite3.sh

---
+++ install_sqlite3.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+sudo apt-get install sqlite3 -y
+sudo apt-get install python-pysqlite2  -y
+sudo apt-get install python-pysqlite2-dbg  -y
+sudo apt-get install libsqlite3-dev  -y 
+sudo apt-get install sqlite  -y
+sudo home/sshuser/venv/bin/pip install pysqlite 
