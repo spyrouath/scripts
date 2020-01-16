2684ad02f6e42815287865d07b4b7d37a76c2329
spyrouath <spyroua@gmail.com>
2020-01-16 11:04:27 +0200

    Update python_env_setup.sh

--- python_env_setup.sh
+++ python_env_setup.sh
@@ -17,7 +17,7 @@ sudo make altinstall
 
 # Create python virtual environmant
 sudo apt install virtualenv -y
-cd ..
+cd /home/sshuser
 wget --no-check-certificate --content-disposition https://raw.githubusercontent.com/spyrouath/scripts/master/venv_requirements.txt -P /home/sshuser
 
 sudo mkdir /home/sshuser/venv
