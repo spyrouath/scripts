df58f2c3f018f55054824a80dbaad27b4cf9adc9
spyrouath <spyroua@gmail.com>
2020-01-22 12:48:22 +0200

    Update reset_python_env.sh

--- reset_python_env.sh
+++ reset_python_env.sh
@@ -5,6 +5,11 @@ sudo rm -rf /home/sshuser/venv
 
 sudo rm /home/sshuser/venv_requirements.*
 
+# Create python virtual environmant
+sudo apt install virtualenv -y
+cd /home/sshuser
+wget --no-check-certificate --content-disposition https://raw.githubusercontent.com/spyrouath/scripts/master/venv_requirements.txt -P /home/sshuser
+
 sudo mkdir /home/sshuser/venv
 sudo virtualenv -p /usr/local/bin/python3.7 /home/sshuser/venv
 sudo chmod 755 /home/sshuser/venv
