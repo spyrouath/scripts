6515de3388e598357173feded6583b5d627a8c73
spyrouath <spyroua@gmail.com>
2020-01-22 12:41:33 +0200

    Update reset_python_env.sh

--- reset_python_env.sh
+++ reset_python_env.sh
@@ -3,3 +3,8 @@
 sudo rm -rf /home/sshuser/venv
 
 sudo rm /home/sshuser/venv_requirements.*
+
+sudo mkdir /home/sshuser/venv
+sudo virtualenv -p /usr/local/bin/python3.7 /home/sshuser/venv
+sudo chmod 755 /home/sshuser/venv
+sudo /home/sshuser/venv/bin/pip3.7 install -r /home/sshuser/venv_requirements.txt
